1277c42471f4529438530041b122c2096fb72885
xyhhx <xyhhx@disr.it>
2023-09-17 00:07:45 -0400

    feat: start on custom theme

--- app/theme.ts
+++ app/theme.ts
@@ -1,10 +1,61 @@
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import {
+	type ChakraTheme,
+	type StyleFunctionProps,
+	type ThemeConfig,
+	defineStyleConfig,
+	extendTheme,
+} from '@chakra-ui/react'
+
+const Card = defineStyleConfig({
+	baseStyle: {
+		container: {
+			borderColor: '#88888888',
+		},
+	},
+	variants: {
+		outline: {
+			boxShadow: 0,
+		},
+	},
+	defaultProps: {
+		variant: 'outline',
+	},
+})
+
+const monoThemeConfig: Pick<
+	ChakraTheme,
+	'styles' | 'semanticTokens' | 'components' | 'colors'
+> = {
+	styles: {
+		global: (props: StyleFunctionProps) => ({
+			body: {
+				bg: props.colorMode === 'dark' ? 'black' : 'white',
+				opacity: 1,
+			},
+			text: {
+				color: props.colorMode === 'dark' ? 'white' : 'black',
+			},
+		}),
+	},
+
+	colors: {},
+
+	components: {
+		Card,
+	},
+	semanticTokens: {
+		colors: {
+			'chakra-body-bg': '#000',
+			'chakra-border-color': '#88888833',
+		},
+	},
+}
 
 const config: ThemeConfig = {
 	initialColorMode: 'system',
 	useSystemColorMode: false,
 }
 
-const theme = extendTheme({ config })
+const theme = extendTheme({ config, ...monoThemeConfig })
 
 export default theme
